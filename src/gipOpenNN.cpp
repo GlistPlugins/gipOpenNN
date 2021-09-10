a803b9373a9c0d49d5bc21c108537cf8d19da79e
R.Noyan Culum <noyanculum@yahoo.com>
2021-09-10 19:08:29 +0300

    Main AI functions and objects

--- src/gipOpenNN.cpp
+++ src/gipOpenNN.cpp
@@ -1,23 +1,44 @@
 /*
  * gipOpenNN.cpp
  *
- *  Created on: 12 Aðu 2021
+ *  Created on: 12 Aï¿½u 2021
  *      Author: oznur
  */
 
 #include "gipOpenNN.h"
 
-using namespace OpenNN;
 
 gipOpenNN::gipOpenNN() {
-
-
 }
 
 gipOpenNN::~gipOpenNN() {
-
+	delete testing_analysis;
+	delete training_strategy;
+	delete neural_network;
+	delete dataset;
 }
 
 void gipOpenNN::setDataset(std::string datasetFilepath, char delimiter, bool hasColumnNames) {
-	dataset = DataSet(gGetFilesDir() + datasetFilepath, delimiter, hasColumnNames);
+	dataset = new Dataset(gGetFilesDir() + datasetFilepath, delimiter, hasColumnNames);
 }
+
+void gipOpenNN::createNeuralNetwork(const NeuralNetwork::ProjectType& projectType, const Tensor<Index, 1>& tensor) {
+	neural_network = new NeuralNetwork(projectType, tensor);
+}
+
+void gipOpenNN::performTraining() {
+	training_strategy = new TrainingStrategy(neural_network, dataset);
+	training_results = training_strategy->perform_training();
+}
+
+void gipOpenNN::calculateTests() {
+	testing_analysis = new TestingAnalysis(neural_network, dataset);
+	binary_classification_tests = testing_analysis->calculate_binary_classification_tests();
+	confusion = testing_analysis->calculate_confusion();
+}
+
+void gipOpenNN::saveResults(std::string neuralNetworkFilename, std::string expressionFileName) {
+	neural_network->save(gGetFilesDir() + neuralNetworkFilename);
+	neural_network->save_expression_python(gGetFilesDir() + expressionFileName);
+}
+
--- src/gipOpenNN.h
+++ src/gipOpenNN.h
@@ -1,7 +1,7 @@
 /*
  * gipOpenNN.h
  *
- *  Created on: 12 Aðu 2021
+ *  Created on: 12 Aï¿½u 2021
  *      Author: oznur
  */
 
@@ -10,18 +10,39 @@
 
 #include "gBasePlugin.h"
 #include "opennn.h"
+#include "optimization_algorithm.h"
 
-// using namespace OpenNN;
 
 class gipOpenNN : public gBasePlugin{
 public:
+
+	typedef OpenNN::DataSet Dataset;
+	typedef OpenNN::NeuralNetwork NeuralNetwork;
+	typedef OpenNN::TrainingStrategy TrainingStrategy;
+	typedef OpenNN::OptimizationAlgorithm OptimizationAlgorithm;
+	typedef OpenNN::TestingAnalysis TestingAnalysis;
+
+	template<typename Scalar_, int NumIndices_>
+	using Tensor = Eigen::Tensor<Scalar_, NumIndices_>;
+
+
 	gipOpenNN();
 	virtual ~gipOpenNN();
 
 	void setDataset(std::string datasetFilepath, char delimiter, bool hasColumnNames);
+	void createNeuralNetwork(const NeuralNetwork::ProjectType&, const Tensor<Index, 1>&);
+	void performTraining();
+	void calculateTests();
+	void saveResults(std::string neuralNetworkFilename, std::string expressionFileName);
 
 private:
-	OpenNN::DataSet dataset;
+	Dataset* dataset;
+	NeuralNetwork* neural_network;
+	TrainingStrategy* training_strategy;
+	OptimizationAlgorithm::Results training_results;
+	TestingAnalysis* testing_analysis;
+	Tensor<float, 1> binary_classification_tests;
+	Tensor<Index, 2> confusion;
 };
 
 #endif /* SRC_GIPOPENNN_H_ */
